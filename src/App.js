de83b139f84d7a614ae89fa8c295230686670fa9
Donmark2k <chukwuemekavp@gmail.com>
2023-03-22 16:35:27 +0100

    Add linters.yml file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/navbar';
+import Countries from './components/country';
+import CountryDetails from './components/countryDetails';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Countries />} />
+        <Route path="/country/:id" element={<CountryDetails />} />
+      </Routes>
     </div>
   );
 }
